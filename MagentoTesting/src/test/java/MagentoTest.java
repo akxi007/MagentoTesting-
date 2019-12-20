b2c21444acc8900f8d4ad8e5450f788f4bc4b2db
ABC <ABC@DESKTOP-1ETOQIP>
2019-12-20 11:59:26 +0000

    first commit

---
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -0,0 +1,30 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class MagentoTest
+{
+	@Test
+	public void magentoTest()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://magento.com");
+		Welcome w = new Welcome(driver);
+		w.clickOnMyAccount();
+		Login l = new Login(driver);
+		l.typeEmail("mzaidurrahman@gmail.com");
+				l.typePassword("Welcome123");
+				
+				
+				
+				l.clickOnLogin();
+				Home h = new Home(driver);
+				h.clickOnLogout();
+		
+		
+		
+	}
+
+}
