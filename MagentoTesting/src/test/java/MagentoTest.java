63596b10f80c7e5e1898a2fcb1a907d0ed295b3d
ABC <ABC@DESKTOP-1ETOQIP>
2019-12-20 12:04:06 +0000

    second commit

--- MagentoTesting/src/test/java/MagentoTest.java
+++ MagentoTesting/src/test/java/MagentoTest.java
@@ -1,14 +1,14 @@
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 public class MagentoTest
 {
 	@Test
 	public void magentoTest()
 	{
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get("https://magento.com");
 		Welcome w = new Welcome(driver);
